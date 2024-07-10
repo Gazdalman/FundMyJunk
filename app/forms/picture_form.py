f4c8e8f56160ed7fd77b6c3e8309b890396101b2
Toney <ta.winston@outlook.com>
2024-07-10 08:49:33 -0500

    Test

--- app/forms/picture_form.py
+++ app/forms/picture_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileAllowed, FileRequired
-from wtforms import StringField, IntegerField, FloatField, TextAreaField, BooleanField, FileField, DateField
+from wtforms import FileField
 from wtforms.validators import DataRequired, Length, ValidationError
 from app.api.aws_helper import ALLOWED_IMG_EXTENSIONS
 
